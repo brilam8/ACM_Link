cc8e6caaab2ad21f247d7eaa3337d4aaf92da69f
Brian Lam <brilam8@gmail.com>
2020-10-22 18:10:43 -0700

    added GET endpoints for database, adjusted endpoint (now at /api/users)

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const request = require('request');
 const fetch = require('node-fetch');
 const app = express();
 const firebase = require("firebase/app");
+const { json } = require('express');
 
 const firebaseConfig = {
   apiKey: "AIzaSyCnpmFxTTaEFuGu-DweMM3sQFH1v_xT7TE",
@@ -30,27 +31,86 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 
+const userCollection = db.collection("users")
+const groupCollection = db.collection("groups")
 
 // @route POST user
-// @desc Creates a user object and stores it in firestore database
-app.post('/api/create_user', (req, res) => {
-  console.log(req.body)
-  const user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email
+// @desc Creates a user object and stores it in the "users" collection in firestore
+app.post('/api/users/create', async (req, res) => {
+  if (!req.body | !req.body.firstName | !req.body.lastName | !req.body.email){
+    res.send("Missing fields on request")
   }
-  db.collection("users").add({
-    user
-  })
-  .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id)
-    res.json(user);
-  })
-  .catch(function(error) {
-    console.error("Error adding document: ", error)
-    res.status(400).send(error)
-  })
+  else {
+    console.log(req.body)
+    const newUserRef = userCollection.doc() // Generate a reference to a doc with unique ID
+    const user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      user_id: newUserRef.id
+    }
+    
+    // Use the previous doc reference to update it with actual information
+    newUserRef.set(user).then( function(){
+      console.log("Document written with ID: ", newUserRef.id)
+      res.json(user);
+    })
+    .catch(function(error) {
+      console.error("Error adding document: ", error)
+      res.status(400).send(error)
+    })
+  }
+})
+
+// @route GET user
+// @desc returns a user object from the "users" collection in firestore given an id
+app.get('/api/users/:userId', async (req, res) => {
+  console.log(req.params)
+  if (!req.params.userId) res.status(400).send("No user id provided");
+  
+  const userRef = await userCollection.doc(req.params.userId);
+  const doc = await userRef.get();
+  if (!doc.exists) {
+    res.status(400).send("No such user document exists");
+  }
+  else {
+    res.json(doc.data());
+  }
+})
+
+// @route GET user(s)
+// @desc returns user object(s) from the "users" collection in firestore given query (or queries)
+app.get('/api/users', async (req, res) => {
+  let querySize = Object.keys(req.query).length; // Check size of queries
+
+  // If no query, retrieve all user objects
+  if (querySize == 0) {
+    
+    let queryResults = await userCollection.get();
+    let results = [];
+    queryResults.forEach(doc => {
+      results.push(doc.data())
+    })
+    res.json(results);
+  }
+  // Else, search by query
+  else {
+  
+    let queryResults;
+    
+    // Go through each query provided and search through database for them
+    for (query in req.query) {
+      console.log(req.query[query])
+      queryResults = userCollection.orderBy(query).startAt(req.query[query]).endAt(req.query[query]+"\uf8ff")
+    }
+    queryResults = await queryResults.get();
+    let results = []; 
+    queryResults.forEach(doc => {
+      results.push(doc.data())
+    })
+    res.json(results);
+  
+  }
 })
 
 app.get('/api/customers', (req, res) => {
