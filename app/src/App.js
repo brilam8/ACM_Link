daf52de8c601cddf07e13e4fe0c13f958ebe7a89
Leland Long <lelandlong@gmail.com>
2020-10-28 19:43:10 -0700

    Added sample code on how to algorithmically add buttons that lead to unique pages

--- app/src/App.js
+++ app/src/App.js
@@ -5,16 +5,21 @@ import {
   Route, 
   Link
 } from 'react-router-dom';
+//import { route } from '../../server/functions/users';
 import sampleComponent from './components/sampleComponent';
 import sampleComponent2 from './components/sampleComponent2';
+import sampleComponent3 from './components/sampleComponent3';
+import buttonPage from './components/buttonPageComponent';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path='/' component={sampleComponent2} />
-        <Route path='/test' component={sampleComponent} /> 
-        <Route component={<div>Page not found</div>} />
+        <Route exact path='/test' component={sampleComponent} /> 
+        <Route exact path='/test2' component={sampleComponent3} /> 
+        <Route exact path='/buttonPage/:UID/:firstName' component={buttonPage} /> 
+        <Route component={() => <div>Page not found</div>} />
       </Switch>
     </Router>
     
