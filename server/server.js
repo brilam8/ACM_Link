29a83ef5a385587ef9de1b00d81b607392686be5
Brian Lam <brilam8@gmail.com>
2020-10-30 16:16:21 -0700

    id token authentication on user routes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const { admin } = require('./firebase');
 const app = express();
 const users = require('./functions/users');
+const cors = require('cors');
 
 // Configure app to use bodyParser
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
+app.use(cors());
 
 //example API call
 app.get('/api/customers', (req, res) => {
@@ -19,8 +22,25 @@ app.get('/api/customers', (req, res) => {
   res.json(customers)
 })
 
+function checkAuth(req, res, next){
+  if (req.headers.authtoken) {
+    admin.auth().verifyIdToken(req.headers.authtoken)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(403).send("Unauthorized")
+    });
+  }
+  else {
+    res.status(403).send("Unauthorized")
+  }
+}
+
 //get all the user routes from users.js
+app.use('/users', checkAuth)
 app.use('/users', users)
+
 
 const port = 5000;
 
