8a45b40bd94c429c1f4a69ebc5b56b9cb2c9e2d7
Tristin Xie <tristinxie@gmail.com>
2020-11-01 00:42:20 -0700

    initial commit

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ import sampleComponent from './components/sampleComponent';
 import sampleComponent2 from './components/sampleComponent2';
 import sampleComponent3 from './components/sampleComponent3';
 import buttonPage from './components/buttonPageComponent';
-
+import createEvent from './components/createEvent'
 function App() {
   return (
     <Router>
@@ -18,6 +18,7 @@ function App() {
         <Route exact path='/' component={sampleComponent2} />
         <Route exact path='/test' component={sampleComponent} /> 
         <Route exact path='/test2' component={sampleComponent3} /> 
+        <Route exact path='/createevent' component={createEvent} />
         <Route exact path='/buttonPage/:UID/:firstName' component={buttonPage} /> 
         <Route component={() => <div>Page not found</div>} />
       </Switch>
