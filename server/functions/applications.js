77b8e14b65eb36d8757bc7d0392adda96d201f4b
Leland Long <lelandlong@gmail.com>
2020-10-31 20:12:04 -0700

    everyone's crud function

--- server/functions/applications.js
+++ server/functions/applications.js
@@ -1,12 +1,79 @@
 const express = require('express');
 const router = express.Router()
 const { json } = require('express');
-const db = require('../firebase');
+const {db, admin} = require('../firebase');
 
 // Configure app to use bodyParser
 router.use(express.urlencoded({
   extended: true
 }));
 router.use(express.json());
+
+// @route POST application
+// @desc Creates an application object and stores it in the 
+//       "application" collection in firestore
+router.post('/send', async (req, res) => {
+  if (!req.body | !req.body.desc) {
+    console.log(req.body);
+    console.log("here2 " + req.body.desc);
+    res.send("Missing fields on request");
+  } else {
+    console.log(req.body);
+    // Generates a reference to a doc with unique ID
+    const newApplicationRef = applyCollection.doc();
+    const application = {
+      app_id: newApplicationRef.id,
+      applicant_id: req.body.applicant_id,
+      event_id: req.body.event_id,
+      comments: req.body.comments,
+      desc: req.body.desc
+    }
+
+    // Use the previously generated reference to update it
+    // with actual data
+    newApplicationRef.set(application).then(function() {
+      console.log("Document written with ID: ", newApplicationRef.id);
+      res.json(application);
+    })
+    .catch(function(error) {
+      console.error("Error adding document ", error);
+      res.status(400).send(error);
+    })
+  }
+});
+
+
+// @route GET application with id
+// @desc returns the application with the given id, or none if 
+//       it doesn't exist
+router.get('/:application_id', async (req, res) => {
+  console.log(req.params);
+  if (!req.params.application_id) {
+    res.status(400).send("No application id specified");
+  }
+
+  const applicationRef = await applyCollection.doc(req.params.application_id);
+  const query = await applicationRef.get();
+  if (!query.exists) {
+    res.status(400).send("No such application exists");
+  } else {
+    res.json(query.data());
+  }
+})
+
+
+
+// TODO Not sure how to add query length
+
+// @route GET applications
+// @desc returns all applications in the database
+router.get('/', async (req, res) => {
+  let queryResults = await applyCollection.get();
+  let results = [];
+  queryResults.forEach(doc => {
+    results = [...results, doc.data()];
+  })
+  res.json(results);
+})
 
 module.exports = router
--- server/functions/events.js
+++ server/functions/events.js
@@ -9,37 +9,85 @@ router.use(express.urlencoded({
 }));
 router.use(express.json());
 
-router.get('/getEvents', async (req, res) => {
-  const eventscollection = db.collection('events').where("event_id", "==", "10000")
-  const query = await eventscollection.get();
-
-  //results are stored in an array
-  let results = [];
-
-  //for each firestore doc returned, store the data into the array
-  query.forEach(doc=>{
-    //same as results.push(doc.data())
-    results = [...results, doc.data()]
+const eventsCollection = db.collection('events')
+//getEvents
+router.get('/', async (req, res) => {
+  const query = await eventsCollection.get()
+  let result = []
+  query.forEach(doc => {
+    result.push(doc.data())
   })
-  console.log(results);
-  res.json(results);
+  res.json(result)
 })
-
-router.get('/setEvents', async (req, res) => {
-  const eventscollection = db.collection('events').doc().set({event_id:"10000"})
-  /*
-  //results are stored in an array
-  let results = [];
-
-  //for each firestore doc returned, store the data into the array
-  query.forEach(doc=>{
-    //same as results.push(doc.data())
-    results = [...results, doc.data()]
+//getEvent by id
+router.get('/:event_id', async (req, res) => {
+  const query = await eventsCollection.where('event_id', '==', req.params.event_id).get()
+  if(query.empty){
+    res.status(400).send('No such event was found')
+  }
+  let result = []
+  query.forEach(doc => {
+    result.push(doc.data())
   })
-  console.log(results);
-  res.json(results);
-  */
- res.json({label:"hi"})
+  res.json(result)
+})
+//getOwnerEvents by id
+router.get('/getOwnerEvents/:creator_id', async (req, res) => {
+  const query = await eventsCollection.where('creator_id', '==', req.params.creator_id).get()
+  if(query.empty){
+    res.status(400).send('No such creator found')
+  }
+  let result = []
+  query.forEach(doc => {
+    result.push(doc.data())
+  })
+  res.json(result)
+})
+//how to differentiate between getuserevent and getevent?
+router.get('/OwnerEvent/:creator_id/:event_id', async (req, res) => {
+  const query = await eventsCollection.where('event_id', '==', req.params.event_id).get()
+  let result = []
+  query.forEach(doc => {
+    result.push(doc.data())
+  })
+  res.json(result)
+})
+//setOwnerEvent status with a boolean value
+router.post('/setOwnerEvent/:event_id/:status', async (req, res) => {
+  const query = await eventsCollection.where('event_id', '==', req.params.event_id).get()
+  if(query.empty) {
+    res.status(400).send('No such event was found')
+  }
+  const event = query.docs[0]
+  event.ref.update({status:req.params.status})
+  res.send(`Event ${req.params.event_id}'s status updated to ${req.params.status}`)
+})
+//creates a new Event
+router.post('/createEvent', async (req, res) => {
+  if(!req.body || !req.body.applications || !req.body.creator_id || !req.body.description || !req.body.end_date || !req.body.event_id || 
+    !req.body.max_applicants || !req.body.start_date || !req.body.status || !req.body.title || !req.body.type){
+
+    //console.log(!req.body, !req.body.applications, !req.body.creator_id, !req.body.description , !req.body.end_date , !req.body.event_id , 
+    //!req.body.max_applicants , !req.body.start_date , !req.body.status , !req.body.title , !req.body.type)
+
+    res.send("Missing fields on request")
+  }else{
+    const event = {
+      applications: req.body.applications,
+      creator_id: req.body.creator_id,
+      description: req.body.description,
+      end_date: req.body.end_date,
+      event_id: req.body.event_id,
+      max_applicants: req.body.max_applicants,
+      start_date: req.body.start_date,
+      status: req.body.status,
+      title: req.body.title,
+      type: req.body.type     
+    }
+
+    eventsCollection.doc().set(event)
+    .then(() => res.json(event))
+  }
 })
 
 module.exports = router
