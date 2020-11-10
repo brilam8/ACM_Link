a07b428b75e4e0a84fd911994da33b269395987c
Brian Lam <brilam8@gmail.com>
2020-11-09 19:46:21 -0800

    removed auth on the entire /users route (need to be able to access the createAccount route)

--- server/server.js
+++ server/server.js
@@ -60,11 +60,7 @@ app.get('/api/getCustomers/:postID/:userID', async (req, res) => {
   res.json(results)
 })
 
-<<<<<<< HEAD
-=======
 //get all the user routes from users.js
->>>>>>> 2ed27bbcd6001af30b7cbf06276d41e082adceee
-//app.use('/users', checkAuth)
 app.use('/users', users)
 app.use('/events', events)
 app.use('/applications', applications)
