69da044c4b45ec0932b9d124269d67e22e06ef34
earghand <eliasarghand@gmail.com>
2020-11-21 19:30:57 -0800

    Added column sorting and event cards

--- app/src/applicationPage/searchComponent.js
+++ app/src/applicationPage/searchComponent.js
@@ -4,9 +4,10 @@ import Button from '@material-ui/core/Button';
 import { Checkbox } from '@material-ui/core';
 import Divider from '@material-ui/core/Divider'
 import { Typography } from '@rmwc/typography'
+import EventCard from '../components/eventCardComponent';
 
 
-function SampleAPI2() {
+function SearchForm() {
   const [output, setOutput] = useState([])
   const [input, setInput] = useState('');
   const [inputSubmit, setInputSubmit] = useState('');
@@ -19,28 +20,14 @@ function SampleAPI2() {
   //Array set to store the filtered events.
   const [filteredEvents, setfilteredEvents] = useState([]);
 
-
-  // Submit function that fetches the data from getAllEvents CRUD function
-  async function filtering() {
-    await handleSubmit();
-    handleFilter();
-  }
-
-  // componentDidMount() {
-  //   fetch("/api/data").then(
-  //       res => this.setState({...this.state, data: res.data})
-  //   )
-  // }
-
-
+  /**
+   * Fetches all the events from the database and stores the elements within an array.
+   */
   async function handleSubmit() {
     setInputSubmit(input);
     console.log("FETCHING");
     const results = await fetch("http://localhost:5000/events/");
-    setResults(await results.json());
-    setInput('');
-    // const filteredEvents = await results.filter(event => event.type.toLowerCase().includes(input.toLowerCase()));
-    // setfilteredEvents([await results.filter(event => event.type.toLowerCase().includes(input.toLowerCase()))]);
+    setResults(await results.json());;
   }
 
   async function handleFilter() {
@@ -48,16 +35,42 @@ function SampleAPI2() {
     const filteredEvents = await results.filter(event => event.type.toLowerCase().includes(input.toLowerCase()));
     setfilteredEvents(await filteredEvents);
   }
+/**
+ * Column filtering based on the homework filter
+ */
+  async function homeworkFilter() {
+    console.log("HANDLING FILTER HOMEWORKL");
+    const filteredEvents = await results.filter(event => event.type.toLowerCase().includes("homework"));
+    setfilteredEvents(await filteredEvents);
+  }
+
+/**
+ * Column filtering based on the games filter
+ */
+  async function gamesFilter() {
+    console.log("HANDLING FILTER GAMES");
+    const filteredEvents = await results.filter(event => event.type.toLowerCase().includes("games"));
+    setfilteredEvents(await filteredEvents);
+  }
+
+/**
+ * Column filtering based on the projects filter.
+ */
+  async function projectsFilter() {
+    console.log("HANDLING FILTER PROJECTS");
+    const filteredEvents = await results.filter(event => event.type.toLowerCase().includes("projects"));
+    setfilteredEvents(await filteredEvents);
+  }
 
   //Filtering Events based on the type with typing
   // const filteredEvents = results.filter(event => event.type.toLowerCase().includes(search.toLowerCase()));
   // const filteredEvents = results.filter(event => event.type.toLowerCase().includes(input.toLowerCase()));
   return (
-    <div className="SampleAPI2">
+    <div className="SearchForm">
       <div style={{ 'marginLeft': '50px' }}>
         <div className="App">
-          <input type="text" placeholder="Search Event Type" onChange={e => setInput(e.target.value)}></input>
-          
+          <input type="text"  placeholder="Search Event Type" onChange={e => setInput(e.target.value)}></input>
+          <h1>Search Posts</h1>
           {filteredEvents.map(event => {
             // event.status = event.status.toString(); <-- Have yet to test
             return (
@@ -67,6 +80,10 @@ function SampleAPI2() {
                 <h2> Description: {event.description}</h2>
                 <h3>Type: {event.type} </h3>
                 <h1> -------------------</h1>
+                <EventCard
+                  user_id= '6PRMGwVGACILQwTnbIHC'        
+                  event_id= {event.event_id}
+                  />
               </>
             )
           })}
@@ -74,42 +91,31 @@ function SampleAPI2() {
         <div>
           {/* Chceckboxes to sort filter. */}
           <p>Games</p>
-          <Checkbox label="Games" />
+          <Checkbox label="Games" onChange={async()=>gamesFilter()}/>
         </div>
         <div>
           <p>Homework</p>
-          <Checkbox label="Homework" />
+          <Checkbox label="Homework" onChange={async()=>homeworkFilter()}/>
         </div>
         <div>
           <p>Projects</p>
-          <Checkbox label="Projects" />
-         
+          <Checkbox label="Projects" onChange={async()=>projectsFilter()}/>
         </div>
         {/* Button to get all the events and calls async handleSubmit function */}
         <Button variant="contained" color="primary" onClick={async () => { await handleSubmit(); await handleFilter(); }}>
           Search Events
         </Button>
       </div>
+   
       <Divider />
       <div style={{ 'marginLeft': '50px' }}>
         {/* Displays the searched text*/}
-        {
-          output.map((text) => {
-            return (
-              <div>
-                <Typography>
-                  Searched Event: {input}
-                </Typography>
-              </div>
-            )
-          })
-        }
       </div>
     </div>
   );
 }
 
-export default SampleAPI2;
+export default SearchForm;
 
 //TODO: 
 //Incorportate the event cards for rickesh
