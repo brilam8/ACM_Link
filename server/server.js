4b7d0a559b7352875037a86894b3d5733f108a3f
Leland Long <lelandlong@gmail.com>
2020-11-25 00:32:58 -0800

    undid checkauth comment

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.get('/api/getCustomers/:postID/:userID', async (req, res) => {
 })
 
 //get all the user routes from users.js
-//app.use('/users', checkAuth)
+app.use('/users', checkAuth)
 app.use('/users', users)
 app.use('/events', events)
 app.use('/applications', applications)
