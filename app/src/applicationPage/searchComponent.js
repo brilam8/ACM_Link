cd8887dea303d5fae8d3168d315a33adadf40f74
earghand <eliasarghand@gmail.com>
2020-12-12 19:43:58 -0800

    Project completion

--- app/src/applicationPage/searchComponent.js
+++ app/src/applicationPage/searchComponent.js
@@ -4,6 +4,12 @@ import { Checkbox, Typography } from '@material-ui/core';
 import Divider from '@material-ui/core/Divider'
 import EventCard from '../components/eventCardComponent';
 import { TextField } from '@rmwc/textfield'
+import { borders } from '@material-ui/system';
+import Box from '@material-ui/core/Box';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
+
 
 function SearchForm() {
   const [input, setInput] = useState('');
@@ -20,97 +26,90 @@ function SearchForm() {
   const [homework, setHomework] = useState(false)
   const [projects, setProjects] = useState(false);
   const [games, setGames] = useState(false);
+  const [date, setDate] = useState(false);
 
 
   /**
    * Fetches all the events from the database and stores the elements within an array.
    */
   async function handleSubmit() {
-    // setInputSubmit(input);
-    console.log("FETCHING");
     setResults([]);
     const response = await fetch("http://localhost:5000/events/");
     const res = await response.json();
     setFilteredEvents([]);
     setResults(res);
-    if(input != '') {
+    if (input != '') {
       handleFilter(res);
     }
-    else if(homework == true) {
+    else if (homework == true) {
       homeworkFilter(res);
     }
-    else if(games == true) {
+    else if (games == true) {
       gamesFilter(res);
     }
-    else if(projects == true) {
+    else if (projects == true) {
       projectsFilter(res);
     }
     else {
       setFilteredEvents(res);
     }
-    if(dateFilter == true) {
+    if (date == true) {
       dateFilter(res);
     }
   }
 
   function handleFilter(unfilteredResults) {
     setFilteredEvents([]);
-    console.log("HANDLING FILTER");
-    console.log("got data length = " + unfilteredResults.length)
-    console.log("FILTER " + input);
     const events = unfilteredResults.filter(event => event.title.toLowerCase().includes(input.toLowerCase()));
     setFilteredEvents(events);
   }
-/**
- * Column filtering based on the homework filter
- */
+  /**
+   * Column filtering based on the homework filter
+   */
   async function homeworkFilter(unfilteredResults) {
-    if(homework == true) {
+    if (homework == true) {
       setFilteredEvents([])
-      console.log("HANDLING FILTER HOMEWORK");
       const filters = unfilteredResults.filter(event => event.type.toLowerCase().includes("homework"));
-      setFilteredEvents(filters); 
+      setFilteredEvents(filters);
     }
   }
 
-/**
- * Column filtering based on the games filter
- */
-async function gamesFilter(unfilteredResults) {
-  if(games == true) {
-    setFilteredEvents([])
-    console.log("HANDLING FILTER HOMEWORK");
-    const filters = unfilteredResults.filter(event => event.type.toLowerCase().includes("games"));
-    setFilteredEvents(filters); 
+  /**
+   * Column filtering based on the games filter
+   */
+  async function gamesFilter(unfilteredResults) {
+    if (games == true) {
+      setFilteredEvents([])
+      const filters = unfilteredResults.filter(event => event.type.toLowerCase().includes("games"));
+      setFilteredEvents(filters);
+    }
   }
-}
-/**
- * Column filtering based on the projects filter.
- */
-async function projectsFilter(unfilteredResults) {
-  if(projects == true) {
-    setFilteredEvents([])
-    console.log("HANDLING FILTER HOMEWORK");
-    const filters = unfilteredResults.filter(event => event.type.toLowerCase().includes("project"));
-    setFilteredEvents(filters); 
+  /**
+   * Column filtering based on the projects filter.
+   */
+  async function projectsFilter(unfilteredResults) {
+    if (projects == true) {
+      setFilteredEvents([])
+      const filters = unfilteredResults.filter(event => event.type.toLowerCase().includes("project"));
+      setFilteredEvents(filters);
+    }
   }
-}
+
 
   function dateFilter(unfilteredResults) {
-      console.log(filteredEvents[0].start_date + "STARTING DATE");
-      // const filteredEvents = unfilteredResults.sort((a,b) => b.start_date["_seconds"] - a.start_date["_seconds"]);
-      let filtered = [];
-      let n = unfilteredResults.length;
-      for(let i = 0; i<unfilteredResults.length(); i++) {
-        for(let j = 0; j<n-i-1; j++) {
-          if(unfilteredResults[j].start_date["_seconds"] > unfilteredResults[j+1].start_date["_seconds"]) {
-            let temp = unfilteredResults[j];
-            unfilteredResults[j] = unfilteredResults[j+1];
-            unfilteredResults[j+1] = temp;
-          }
+    // const filteredEvents = unfilteredResults.sort((a,b) => b.start_date["_seconds"] - a.start_date["_seconds"]);
+    let filtered = [];
+    let n = unfilteredResults.length;
+    for (let i = 0; i < unfilteredResults.length(); i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (unfilteredResults[j].start_date["_seconds"] > unfilteredResults[j + 1].start_date["_seconds"]) {
+          let temp = unfilteredResults[j];
+          unfilteredResults[j] = unfilteredResults[j + 1];
+          unfilteredResults[j + 1] = temp;
         }
       }
-      setFilteredEvents(unfilteredResults);
+    }
+    setFilteredEvents(unfilteredResults);
   }
 
   //Filtering Events based on the type with typing
@@ -119,89 +118,130 @@ async function projectsFilter(unfilteredResults) {
     <div className="SearchForm">
       <div style={{ 'marginLeft': '50px' }}>
         <div className="App">
-        <TextField 
-            style = {{
+          <TextField
+            style={{
               width: "25%",
-              margin: "2.5% 0% 2.5% -2%",
-              color:"black"
-            }} 
-            value = {input}
+              margin: "2.5% 0% 2.5% 0%",
+              color: "black"
+            }}
+            value={input}
             label="Search posts"
             onChange={e => setInput(e.target.value)}
-        />
-        <Button variant="contained" style = {{margin: "2.5% 0% 2.5% 1%"}} color="primary" onClick= { async()=> { 
-          await handleSubmit(); 
-        }}>
-          Search Events
+          />
+          <Button variant="contained" style={{ margin: "2.5% 0% 2.5% 1%" }} color="primary" onClick={async () => {
+            await handleSubmit();
+          }}>
+            Search Events
         </Button>
-          <Typography>
-            FILTERS:
-          </Typography>
-          {/* Chceckboxes to sort filter. */}
-          <Typography>
-            Type:
-          </Typography>
-          <p>Games</p>
-          <Checkbox label="Games" onChange={()=> {
-            if(games == false) {
-              setGames(true);
-            }
-            else {
-              setGames(false);
-            }
-            }}/>
-        <div>
-          <p>Homework</p>
-          <Checkbox label="Homework" onChange={()=> {
-            if(homework == false) {
-              setHomework(true);
-            }
-            else {
-              setHomework(false);
-            }
-            }}/>
-            
-        </div>
-        <div>
-          <p>Projects</p>
-          <Checkbox label="Projects" onChange={()=> {
-            if(projects == false) {
-              setProjects(true);
-            }
-            else {
-              setProjects(false);
-            }
-            }}/>
-        </div>
-          <Typography style = {{margin: "2.5% 0% 1.5% 0%", color:"Black",}} use="headline3">
-            Sort by
-            </Typography>
-            <Typography>
-              Date:
-            </Typography>
-            <Checkbox label = "Date" > </Checkbox>
-            <div style={{display: 'flex', alignItems: 'center', marginTop: 0, flexWrap: 'wrap'}}>
-        
-          {filteredEvents.map(event => {
-            // event.status = event.status.toString(); <-- Have yet to test
-            //console.log(event.creator_id)
-            console.log(event.event_id)
-            return (
-              // Printing the title and description of each event, along with the status
-              
-              <>
-                <EventCard
-                  user_id = {event.creator_id}       
-                  event_id= {event.event_id}
+          {/* <div style={{display: 'flex', alignItems: 'center', marginTop: 0, flexWrap: 'wrap'}}> */}
+          {/* <div className="huge-container" style={{display: 'flex', width: '100%',}}> */}
+          <div className="left-side" style={{}}>
+            <Box
+              border={2}
+              alignItems="center"
+              justifyContent="center"
+              style={{ width: '10%' }}
+            >
+              <Typography>
+                <Box
+                  bgcolor="#212121"
+                  borderBottom={2}
+                  borderTop={2}
+                >
+                  <h3 style={{ color: 'white', textAlign: 'center' }}>FILTERS:</h3>
+                </Box>
+              </Typography>
+              {/* Chceckboxes to sort filter. */}
+              <Typography>
+                <h3 style={{ textAlign: 'center', margin: "2.5% 0% 2.5% 1%" }}>Type:</h3>
+              </Typography>
+              {/* <p>Games</p> */}
+              <FormControlLabel
+                control={
+                  <Checkbox label="Games" name="Games" onChange={() => {
+                    if (games == false) {
+                      setGames(true);
+                    }
+                    else {
+                      setGames(false);
+                    }
+                  }} />
+                }
+                label="Games"
+              />
+              <div>
+                <FormControlLabel
+                  label="Homework"
+                  control={
+                    <Checkbox onChange={() => {
+                      if (homework == false) {
+                        setHomework(true);
+                      }
+                      else {
+                        setHomework(false);
+                      }
+                    }} />
+                  }
+                />
+
+              </div>
+              <div>
+                <FormControlLabel
+                  control={
+                    <Checkbox label="Projects" onChange={() => {
+                      if (projects == false) {
+                        setProjects(true);
+                      }
+                      else {
+                        setProjects(false);
+                      }
+                    }} />
+                  }
+                  label="Projects"
+                />
+              </div>
+              <Typography>
+                <h3 style={{ textAlign: 'center', margin: "3% 0% 1.5% 0%" }}>Sort By:</h3>
+              </Typography>
+              {/* </Box> */}
+              {/* </div> */}
+              <div style={{ display: 'flex', alignItems: 'center', marginTop: 0, flexWrap: 'wrap' }}>
+                <FormControlLabel
+                  control={
+                    <Checkbox label="Date"> onChange={() => {
+                      if (date == false) {
+                        setDate(true);
+                      }
+                      else {
+                        setDate(false);
+                      }
+                    }}/</Checkbox>
+                  }
+                  label="Date"
+                />
+              </div>
+            </Box>
+          </div>
+          {/* <div className="right-side" style={{ 'flex': 2, 'width': '100%', }}> */}
+          <div style={{ display: 'flex', alignItems: 'center', marginTop: 0, flexWrap: 'wrap' }}>
+            {filteredEvents.map(event => {
+              return (
+                // Printing the title and description of each event, along with the status
+
+                <>
+                  <EventCard
+                    user_id={event.creator_id}
+                    event_id={event.event_id}
                   />
                   {/* <h1>{ event.start_date["_seconds"] }</h1> */}
-              </>
-            )
-          })}
+                </>
+              )
+            })}
           </div>
         </div>
       </div>
     </div>
+    // </div>
   );
 }
 
